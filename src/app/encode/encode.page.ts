f357c441bee7e9415393267fe8afc434905e94e4
Risto Kurniawan <risto9f@gmail.com>
2019-12-25 20:53:39 +0700

    Belum Coba

--- src/app/encode/encode.page.ts
+++ src/app/encode/encode.page.ts
@@ -297,5 +297,51 @@ Crypter = {
 	},
 };
 
+saveimage(){
+  let base64ImageData; 
+  var gambar=this.Result.context.getImageData(0,0, this.Result.el.width, this.Result.el.height);
+  base64ImageData = 'data:image/jpeg;base64,' + gambar;  
+  this.writeFile(base64ImageData, 'My Picture', 'sample.jpeg');  
+}
+
+ //here is the method is used to write a file in storage  
+ public writeFile(base64Data: any, folderName: string, fileName: any) {  
+  let contentType = this.getContentType(base64Data);  
+  let DataBlob = this.base64toBlob(base64Data, contentType);  
+  // here iam mentioned this line this.file.externalRootDirectory is a native pre-defined file path storage. You can change a file path whatever pre-defined method.  
+  let filePath = this.file.externalRootDirectory + folderName;  
+  this.file.writeFile(filePath, fileName, DataBlob, contentType).then((success) => {  
+      console.log("File Writed Successfully", success);  
+  }).catch((err) => {  
+      console.log("Error Occured While Writing File", err);  
+  })  
+}  
+//here is the method is used to get content type of an bas64 data  
+public getContentType(base64Data: any) {  
+  let block = base64Data.split(";");  
+  let contentType = block[0].split(":")[1];  
+  return contentType;  
+}  
+//here is the method is used to convert base64 data to blob data  
+public base64toBlob(b64Data, contentType) {  
+  contentType = contentType || '';  
+  const sliceSize = 512;  
+  let byteCharacters = atob(b64Data);  
+  let byteArrays = [];  
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {  
+      let slice = byteCharacters.slice(offset, offset + sliceSize);  
+      let byteNumbers = new Array(slice.length);  
+      for (let i = 0; i < slice.length; i++) {  
+          byteNumbers[i] = slice.charCodeAt(i);  
+      }  
+      var byteArray = new Uint8Array(byteNumbers);  
+      byteArrays.push(byteArray);  
+  }  
+  let blob = new Blob(byteArrays, {  
+      type: contentType  
+  });  
+  return blob;  
+}  
+
 
 }
